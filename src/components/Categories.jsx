bec36fee8cc04a2703c5d94d05d6f62e7279cbc1
Daniel <koldan2001@gmail.com>
2022-07-18 21:44:18 +0200

    Init project

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meaty</li>
+        <li>Vegetarian</li>
+        <li>Grilled</li>
+        <li>Spicy</li>
+        <li>Folded</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
